65efe30537db0e89ab8e4184e3232ae4b78a5106
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:18:54 +0530

    Update exist_key.py

--- exist_key.py
+++ exist_key.py
@@ -7,3 +7,16 @@ def is_key_present(x):
       print('Key is not present in the dictionary')
 is_key_present(5)
 is_key_present(9)
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
