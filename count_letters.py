58668416515ded8ce95e38323f3df195ac6ab631
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:16:34 +0530

    Update count_letters.py

--- count_letters.py
+++ count_letters.py
@@ -11,3 +11,15 @@ print("The number of lowercase characters is:")
 print(count1)
 print("The number of uppercase characters is:")
 print(count2)
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
