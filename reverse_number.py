d73d85332f4eef5d0aa2628fd3b09191fe0e8e47
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:25:08 +0530

    Update reverse_number.py

--- reverse_number.py
+++ reverse_number.py
@@ -5,3 +5,22 @@ rev=0 while(n>0):
  rev=rev*10+dig
  n=n//10
  print("Reverse of the number:",rev)
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
