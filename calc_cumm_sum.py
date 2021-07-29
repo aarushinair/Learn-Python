0f17acb1e7a7e7d467cf9bf76491145d1618e621
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 10:05:00 +0530

    Update calc_cumm_sum.py

--- calc_cumm_sum.py
+++ calc_cumm_sum.py
@@ -1,4 +1,5 @@
-#Write a NumPy program to calculate cumulative sum of the elements along a given axis, sum over rows for each of the 3 columns and sum over columns for each of the 2 rows of a given 3x3 array.
+#Write a NumPy program to calculate cumulative sum of the elements along a given axis, 
+#sum over rows for each of the 3 columns and sum over columns for each of the 2 rows of a given 3x3 array.
 import numpy as np
 x = np.array([[1,2,3], [4,5,6]])
 print("Original array: ")
