460112db477f8e6ff2841adb5c50cffe2c505caf
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:26:04 +0530

    Update sort_values.py

--- sort_values.py
+++ sort_values.py
@@ -26,3 +26,16 @@ def main():
 # Main function calling 
 if _name=="main_":       
     main()
+
+    
+    
+    
+    
+    
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
