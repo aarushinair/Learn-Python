14060cd6472cd6e75e165e7711589e80c314e2c9
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:25:25 +0530

    Update reverse_words.py

--- reverse_words.py
+++ reverse_words.py
@@ -4,3 +4,20 @@ def reverse_string_words(text):
         return(' '.join(line.split()[::-1]))
 print(reverse_string_words("The quick brown fox jumps over the lazy dog."))
 print(reverse_string_words("Python Exercises."))
+
+
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
