3f13a938ca975cfac8145e4a8d8cc5609c3db39c
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:21:16 +0530

    Update merge_dictionaries.py

--- merge_dictionaries.py
+++ merge_dictionaries.py
@@ -4,3 +4,18 @@ d2 = {'x': 300, 'y': 200}
 d = d1.copy()
 d.update(d2)
 print(d)
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
