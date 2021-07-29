3d8099295f085a66bb9e3121abe1afa6f6ddf9d0
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:24:38 +0530

    Update repeat_char.py

--- repeat_char.py
+++ repeat_char.py
@@ -8,3 +8,18 @@ for c in str1:
 for c in sorted(d, key=d.get, reverse=True):
   if d[c] > 1:
       print('%s %d' % (c, d[c]))
+
+        
+        
+        
+        
+        
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
