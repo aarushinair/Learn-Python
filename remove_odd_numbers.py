4d0d1a3f9da3d5ac4e762880a9892f0d135ef5d5
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:23:22 +0530

    Update remove_odd_numbers.py

--- remove_odd_numbers.py
+++ remove_odd_numbers.py
@@ -15,3 +15,18 @@ for i  in list:
 # print list after removing ODD elements
 print "list after removing ODD numbers:"
 print list
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
