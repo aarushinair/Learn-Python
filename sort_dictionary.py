96c1bcdbee5479f6af4b39d9f9b9021c2ac78804
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 09:55:25 +0530

    Create sort_dictionary.py

---
+++ sort_dictionary.py
@@ -0,0 +1,7 @@
+#Write a Python program to sort a dictionary by key.
+color_dict = {'red':'#FF0000',
+          'green':'#008000',
+          'black':'#000000',
+          'white':'#FFFFFF'
+for key in sorted(color_dict):
+  print("%s: %s" % (key, color_dict[key]))
