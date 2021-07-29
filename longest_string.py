3902c246ef11471b94197f452fb2249b22322bea
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:20:34 +0530

    Update longest_string.py

--- longest_string.py
+++ longest_string.py
@@ -19,3 +19,16 @@ if _name_ == "_main_" :
     n = len(X) 
     m = len(Y) 
     print(lcs(n, m, 0))
+
+    
+    
+    
+    
+    
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
