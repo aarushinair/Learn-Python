005ba4e7823b3006a5c4f41adddb0957e2de9f0e
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:19:35 +0530

    Update find_word.py

--- find_word.py
+++ find_word.py
@@ -24,3 +24,15 @@ print("Original Strings:\n",str1)
 small, large = smallest_largest_words(str1)  
 print("Smallest word: " + small);  
 print("Largest word: " + large);
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
