44061819a43551f93303f280db55ad5453f1da0f
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:20:19 +0530

    Update intersection.py

--- intersection.py
+++ intersection.py
@@ -8,3 +8,16 @@ print("Remove the intersection of a 2nd set from the 1st set using difference_up
 print(sn1.difference(sn2))
 print("Remove the intersection of a 2nd set from the 1st set using -= operator:")
 print(sn1-sn2)
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
