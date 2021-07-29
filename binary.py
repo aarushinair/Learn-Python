1409b591bff069adc69117670dfd08b4afcc2d25
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:15:12 +0530

    Update binary.py

--- binary.py
+++ binary.py
@@ -7,3 +7,15 @@ for i in range(len(b_num)):
 	if digit == '1':
 		value = value + pow(2, i)
 print("The decimal value of the number is", value)
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
