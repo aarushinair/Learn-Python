c19ab346f9133d3311d6e5501f78f9d3eea5548c
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 09:50:11 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,10 @@
+#Easy quick functions on a list:
+L1=[12,23,23,56,67,24] 
+(a) find number of elements in the list
+        print(len(1))
+ (b) remove 3rd element
+L1.remove(23)
+ (c) add 33 at index position 0 
+L1.insert(0,33)
+d) add 99 at the end of the list
+L1.append(99)
