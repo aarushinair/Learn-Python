a4dcf6d6b4aa7833aae3e2028538de1ba30f962c
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 09:42:09 +0530

    Create count_letters.py

---
+++ count_letters.py
@@ -0,0 +1,13 @@
+#Write a program to count total number of upper case and lower case letters in it.
+string=raw_input("Enter string:")
+count1=0
+count2=0
+for i in string:
+      if(i.islower()):
+            count1=count1+1
+      elif(i.isupper()):
+            count2=count2+1
+print("The number of lowercase characters is:")
+print(count1)
+print("The number of uppercase characters is:")
+print(count2)
