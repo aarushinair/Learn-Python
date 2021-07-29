f96a9108d0b86f0c6115428fa70f22922aafc788
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:18:38 +0530

    Update create_matrix.py

--- create_matrix.py
+++ create_matrix.py
@@ -3,3 +3,16 @@ import numpy as np
 x = np.ones((10, 10))
 x[1:-1, 1:-1] = 0
 print(x)
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
