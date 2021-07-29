5272ea9ce4dba9241ae039c56ba9b73dd7b46804
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:26:24 +0530

    Update sum_of_all.py

--- sum_of_all.py
+++ sum_of_all.py
@@ -9,3 +9,17 @@ print("Sum of each column:")
 print(np.sum(x, axis=0))
 print("Sum of each row:")
 print(np.sum(x, axis=1))
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
