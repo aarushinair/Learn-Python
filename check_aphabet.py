10a2e36ace48f9d591d1b9284f2bc5d51c5c1fdf
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 09:45:55 +0530

    Create check_aphabet.py

---
+++ check_aphabet.py
@@ -0,0 +1,9 @@
+#Write a Python program to check if a string contains all letters of the alphabet.
+import string
+alphabet = set(string.ascii_lowercase)
+input_string = 'The quick brown fox jumps over the lazy dog'
+print(set(input_string.lower()) >= alphabet)
+#the above output = true
+input_string = 'The quick brown fox jumps over the lazy cat'
+print(set(input_string.lower()) >= alphabet)
+#the above output = false
