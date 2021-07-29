c5e8cc9635bf6e57cc5f26a24bc5adb60ac8e958
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:20:05 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -8,3 +8,15 @@ L1.remove(23)
 L1.insert(0,33)
 d) add 99 at the end of the list
 L1.append(99)
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
