96b39c969ad7f26a0339b31ac2a6676ed8b3b8b5
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 09:51:40 +0530

    Create remove_item.py

---
+++ remove_item.py
@@ -0,0 +1,6 @@
+#Write a Python program to remove an item from a set if it is present in the set.
+#Create a new set
+num_set = set([0, 1, 2, 3, 4, 5])
+#Discard number 4
+num_set.discard(4)
+print(num_set)
