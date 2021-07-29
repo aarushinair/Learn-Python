22e7d102321f0d136610c01e31b9c5e443b141ad
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 09:43:25 +0530

    Create remove_char.py

---
+++ remove_char.py
@@ -0,0 +1,25 @@
+#Write a Python3 program for removing i-th indexed character from a string.
+
+def remove(string, i): 
+
+	# Characters before the i-th indexed 
+	# is stored in a variable a 
+	a = string[ : i] 
+	
+	# Characters after the nth indexed 
+	# is stored in a variable b 
+	b = string[i + 1: ] 
+	
+	# Returning string after removing 
+	# nth indexed character. 
+	return a + b 
+# Driver Code 
+if _name_ == '_main_': 
+	
+	string = "nikki"
+	
+	# Remove nth index element 
+	i = 5
+	
+	# Print the new string 
+	print(remove(string, i))
