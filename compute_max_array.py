2f83802071e10478350a286539e8c94db8cb2ad3
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 10:02:06 +0530

    Create compute_max_array.py

---
+++ compute_max_array.py
@@ -0,0 +1,30 @@
+#Compute the maximum for each row in the given array?
+import numpy 
+  
+# Function to get max element 
+def maxelement(arr): 
+      
+    # get number of rows and columns 
+    no_of_rows = len(arr) 
+    no_of_column = len(arr[0]) 
+      
+    for i in range(no_of_rows): 
+          
+        # Initialize max1 to 0 at beginning 
+        # of finding max element of each row 
+        max1 = 0
+        for j in range(no_of_column): 
+            if arr[i][j] > max1 : 
+                max1 = arr[i][j] 
+                  
+        # print maximum element of each row 
+        print(max1) 
+  
+# Driver Code 
+arr = [[3, 4, 1, 8], 
+       [1, 4, 9, 11], 
+       [76, 34, 21, 1], 
+       [2, 1, 4, 5]] 
+  
+# Calling the function         
+maxelement(arr)
