a500f21859b8582e1f32c9bed1f56eb459731e06
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:15:57 +0530

    Update check_aphabet.py

--- check_aphabet.py
+++ check_aphabet.py
@@ -7,3 +7,14 @@ print(set(input_string.lower()) >= alphabet)
 input_string = 'The quick brown fox jumps over the lazy cat'
 print(set(input_string.lower()) >= alphabet)
 #the above output = false
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
