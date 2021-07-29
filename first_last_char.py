d5dde7c9b1a2512ab1fed77e5490ba4b8c8d3951
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 20:19:50 +0530

    Update first_last_char.py

--- first_last_char.py
+++ first_last_char.py
@@ -9,3 +9,15 @@ def no_of_substring_with_equalEnds(str1):
 	return result 
 str1 = input("Input a string: ")
 print(no_of_substring_with_equalEnds(str1))
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
