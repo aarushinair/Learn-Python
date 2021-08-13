3b77102cb5d08c744e70184070324f641658dcd9
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-13 16:27:31 +0530

    Update count_words.py

--- count_words.py
+++ count_words.py
@@ -12,7 +12,6 @@ number_words(s)
 
 
 
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
