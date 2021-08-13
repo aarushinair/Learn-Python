e5e5e361f3c530e16be845661603c6b8c5d25625
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-13 16:25:45 +0530

    Update binary.py

--- binary.py
+++ binary.py
@@ -13,6 +13,7 @@ print("The decimal value of the number is", value)
 
 
 
+
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
