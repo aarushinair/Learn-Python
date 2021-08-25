9c01bb60f5108cef74983df38898e89c160ef554
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-25 17:49:59 +0530

    Update req_value.py

--- req_value.py
+++ req_value.py
@@ -1,5 +1,5 @@
 #Write a NumPy program to find the most frequent value in an array x given below.
-a=[1,2,3,4,3,2,4,5,3,3]
+
 import numpy as np
 x = np.random.randint(0, 10, 40)
 print("Original array:")
