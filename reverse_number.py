0cebb0fa9f6b1ec14fe2fc8488179d35c3fa9167
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-11-24 13:48:23 +0530

    Update reverse_number.py

--- reverse_number.py
+++ reverse_number.py
@@ -14,7 +14,6 @@ rev=0 while(n>0):
  
  
  
- 
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
