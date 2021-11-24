cf1c636f2190bb81ef702de7f58ab7acf7063de2
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-11-24 13:49:11 +0530

    Update vowels_replace.py

--- vowels_replace.py
+++ vowels_replace.py
@@ -19,8 +19,6 @@ rem_vowel(string)
 
 
 
-
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
