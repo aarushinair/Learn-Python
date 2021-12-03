dab0ddde84db5961fb04ae97390498b68c24ae42
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-12-03 12:36:56 +0530

    Update check_aphabet.py

--- check_aphabet.py
+++ check_aphabet.py
@@ -11,8 +11,6 @@ print(set(input_string.lower()) >= alphabet)
 
 
 
-
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
