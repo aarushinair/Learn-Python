14fc3c09237a12a083ce0c201ede32efec85cd13
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-12-04 13:58:02 +0530

    Update calc_cumm_sum.py

--- calc_cumm_sum.py
+++ calc_cumm_sum.py
@@ -18,6 +18,8 @@ print(r)
 
 
 
+
+
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
