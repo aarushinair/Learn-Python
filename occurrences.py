63cbbb68da09837655badd9cb0f454f0c2ba2b60
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-01-20 10:46:12 +0530

    Update occurrences.py

--- occurrences.py
+++ occurrences.py
@@ -17,8 +17,6 @@ print(Counter(words).most_common(10))
 
 
 
-
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
