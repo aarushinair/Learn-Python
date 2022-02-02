bba963f40b231b440ec99acb42d0cd56950f84f7
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-02-02 09:25:32 +0530

    Update remove_char.py

--- remove_char.py
+++ remove_char.py
@@ -31,6 +31,8 @@ if _name_ == '_main_':
 	
 	
 	
+	
+	
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
