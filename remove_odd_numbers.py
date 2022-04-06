cb825ed08c98d74380cd1b828dc2de6659960f73
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-04-06 11:21:52 +0530

    Update remove_odd_numbers.py

--- remove_odd_numbers.py
+++ remove_odd_numbers.py
@@ -21,6 +21,8 @@ print list
 
 
 
+
+
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
