611e2e57fe78a079acce139af6caaf73db70843f
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-05-12 11:43:08 +0530

    Update inorderTraversal.py

--- inorderTraversal.py
+++ inorderTraversal.py
@@ -26,3 +26,18 @@ root.left.right = Node(5)
 
 print "\nInorder traversal of binary tree is"
 printInorder(root)
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
