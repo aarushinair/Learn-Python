3910557d2694a47ba19fac9b1f2e5f89177d2673
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-05-13 11:28:57 +0530

    Update preorderTraversal.py

--- preorderTraversal.py
+++ preorderTraversal.py
@@ -25,3 +25,18 @@ root.left.left = Node(4)
 root.left.right = Node(5)
 print "Preorder traversal of binary tree is"
 printPreorder(root)
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
