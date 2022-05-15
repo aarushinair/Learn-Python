a6dc0241433928ef36c7315902320ad9313cd6f1
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-05-15 11:34:48 +0530

    Update postorderTraversal.py

--- postorderTraversal.py
+++ postorderTraversal.py
@@ -23,3 +23,18 @@ root.left.left = Node(9)
 root.left.right = Node(10)
 print "\nPostorder traversal of binary tree is"
 printPostorder(root)
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
