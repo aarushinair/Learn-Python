7209d9f47a96ec079e77fa98ae2c6c10e2f3e475
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-05-26 13:00:57 +0530

    Create decimalToHexadecimal.py

---
+++ decimalToHexadecimal.py
@@ -0,0 +1,20 @@
+#Convert decimal to hexadecimal without using built-in hex() function
+
+conversion_table = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4',
+                    5: '5', 6: '6', 7: '7',
+                    8: '8', 9: '9', 10: 'A', 11: 'B', 12: 'C',
+                    13: 'D', 14: 'E', 15: 'F'}
+  
+ 
+def decimalToHexadecimal(decimal):
+    hexadecimal = ''
+    while(decimal > 0):
+        remainder = decimal % 16
+        hexadecimal = conversion_table[remainder] + hexadecimal
+        decimal = decimal // 16
+  
+    return hexadecimal
+  
+#driver code
+decimal_number = 69
+print("The hexadecimal form of", decimal_number,"is", decimalToHexadecimal(decimal_number))
