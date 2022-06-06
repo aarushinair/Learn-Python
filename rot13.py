9bbce8c85dfb012bef018ca54c083111895c7e50
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-06-06 23:09:21 +0530

    Create rot13.py

---
+++ rot13.py
@@ -0,0 +1,33 @@
+# Dictionary to lookup the index of alphabets
+dict1 = {'A' : 1, 'B' : 2, 'C' : 3, 'D' : 4, 'E' : 5,
+        'F' : 6, 'G' : 7, 'H' : 8, 'I' : 9, 'J' : 10,
+        'K' : 11, 'L' : 12, 'M' : 13, 'N' : 14, 'O' : 15,
+        'P' : 16, 'Q' : 17, 'R' : 18, 'S' : 19, 'T' : 20,
+        'U' : 21, 'V' : 22, 'W' : 23, 'X' : 24, 'Y' : 25, 'Z' : 26}
+ 
+dict2 = {0 : 'Z', 1 : 'A', 2 : 'B', 3 : 'C', 4 : 'D', 5 : 'E',
+        6 : 'F', 7 : 'G', 8 : 'H', 9 : 'I', 10 : 'J',
+        11 : 'K', 12 : 'L', 13 : 'M', 14 : 'N', 15 : 'O',
+        16 : 'P', 17 : 'Q', 18 : 'R', 19 : 'S', 20 : 'T',
+        21 : 'U', 22 : 'V', 23 : 'W', 24 : 'X', 25 : 'Y'}
+ 
+def encrypt(message, shift):
+    cipher = ''
+    for letter in message:
+        if(letter != ' '):
+            num = ( dict1[letter] + shift ) % 26
+            cipher += dict2[num]
+        else:
+            cipher += ' '
+ 
+    return cipher
+    
+# driver function to run the program
+def main():
+    message = "GEEKS FOR GEEKS"
+    shift = 13
+    result = encrypt(message.upper(), shift)
+    print (result)
+
+if __name__ == '__main__':
+    main()
