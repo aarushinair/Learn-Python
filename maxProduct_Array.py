7dcbcae173fa0af6ccfbf5d1a4f808a41a4856eb
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-06-13 07:19:15 +0530

    Update maxProduct_Array.py

--- maxProduct_Array.py
+++ maxProduct_Array.py
@@ -19,3 +19,18 @@ def maxArrayProduct(arr, n):
 arr = [ 5, -1, -4, 0, 2, -6, -1 ]
 n = len(arr)
 print("Maximum product in Sub Array is" , maxArrayProduct(arr, n))
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
