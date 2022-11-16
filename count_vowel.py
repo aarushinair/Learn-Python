5fe9134907cd7ca413111d02599f42e5f0ab2ef5
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-11-16 21:03:30 +0530

    Update count_vowel.py

--- count_vowel.py
+++ count_vowel.py
@@ -20,6 +20,11 @@ print "Total vowels are: ", count
 
 
 
+
+
+
+
+
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
