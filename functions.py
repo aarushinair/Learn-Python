30230198ace2f340a7058132bbd39fa1dadda414
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-12-22 09:58:44 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -11,10 +11,6 @@ L1.append(99)
 
 
 
-
-
-
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
