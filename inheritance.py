08d05bb1650a1a5e776d0a0453ba9a1de81f0986
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2023-02-01 19:45:25 +0530

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -33,9 +33,7 @@ t.findArea()
 
 
 
-
-
-
+"""
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -43,3 +41,4 @@ t.findArea()
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+"""
