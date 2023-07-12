f9cb2a93bec6b4a084f6c090014d37e68d3809e2
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2023-07-12 23:47:37 +0530

    Update accept.py

--- accept.py
+++ accept.py
@@ -22,7 +22,7 @@ for i in high:
 
 
 
-
+"""
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -30,3 +30,4 @@ for i in high:
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+"""
