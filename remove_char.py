de412168367dea734d9fcc73e6d43b8b5da54947
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2023-07-21 08:38:19 +0530

    Update remove_char.py

--- remove_char.py
+++ remove_char.py
@@ -32,7 +32,7 @@ if _name_ == '_main_':
 	
 	
 	
-	
+"""	
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -40,3 +40,4 @@ if _name_ == '_main_':
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+"""
