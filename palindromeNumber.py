cdc43d5de1cc8481f94b874b64be3f95b689d81f
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2023-07-24 17:11:09 +0530

    Update palindromeNumber.py

--- palindromeNumber.py
+++ palindromeNumber.py
@@ -25,7 +25,7 @@ print ("Is the number palindrome : " + str(res))
 
 
 
-
+"""
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -33,3 +33,4 @@ print ("Is the number palindrome : " + str(res))
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+"""
