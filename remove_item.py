4760768d8df565e46b69d0ee8bcbeed0d49f81b0
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2023-08-18 11:11:11 +0530

    Update remove_item.py

--- remove_item.py
+++ remove_item.py
@@ -12,7 +12,7 @@ print(num_set)
 
 
 
-
+"""
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -20,3 +20,4 @@ print(num_set)
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+"""
