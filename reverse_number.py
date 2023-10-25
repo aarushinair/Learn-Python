58a3237a7b18e689a9883e08da3d0dafcee933eb
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2023-10-25 07:37:13 +0530

    Update reverse_number.py

--- reverse_number.py
+++ reverse_number.py
@@ -18,7 +18,7 @@ rev=0 while(n>0):
  
  
  
- 
+ """
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -26,3 +26,4 @@ rev=0 while(n>0):
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+"""
