fefae273d997358bcdc93f54fb82eb98543c4164
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2024-01-10 22:01:16 +0530

    Update sort_dictionary.py

--- sort_dictionary.py
+++ sort_dictionary.py
@@ -7,7 +7,7 @@ for key in sorted(color_dict):
   print("%s: %s" % (key, color_dict[key]))
 
               
-          
+"""          
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -15,3 +15,4 @@ for key in sorted(color_dict):
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+"""
