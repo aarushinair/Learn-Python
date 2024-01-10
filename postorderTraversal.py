858118ef751323cdc42311724b0b424235ead255
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2024-01-10 21:59:45 +0530

    Update postorderTraversal.py

--- postorderTraversal.py
+++ postorderTraversal.py
@@ -29,8 +29,7 @@ printPostorder(root)
 
 
 
-
-
+"""
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -38,3 +37,4 @@ printPostorder(root)
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+"""
