23fbddf1a620278c52e8e2ea56a17b233f137270
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2024-03-02 10:14:27 +0530

    Update sumString.py

--- sumString.py
+++ sumString.py
@@ -24,7 +24,7 @@ print (calc(input()))
 
 
 
-
+"""
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -32,3 +32,4 @@ print (calc(input()))
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+"""
