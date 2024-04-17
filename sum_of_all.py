4e6eedc5ac1b6f4332f7fca39895f5d0537f844a
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2024-04-17 23:32:21 +0530

    Update sum_of_all.py

--- sum_of_all.py
+++ sum_of_all.py
@@ -17,7 +17,7 @@ print(np.sum(x, axis=1))
 
 
 
-
+"""
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -25,3 +25,4 @@ print(np.sum(x, axis=1))
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+"""
