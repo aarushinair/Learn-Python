4202e3155dd9281b8357a069eaf75faed053a47f
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2024-05-15 00:03:09 +0530

    Update swap_case.py

--- swap_case.py
+++ swap_case.py
@@ -16,7 +16,7 @@ print(swap_case_string("AaRUsHi aNEesH nAIr"))
 
 
 
-
+"""
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -24,3 +24,4 @@ print(swap_case_string("AaRUsHi aNEesH nAIr"))
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+"""
