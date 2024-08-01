bac7abd1a12921d8d7130ec9060b2150839d6b43
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2024-08-01 11:59:20 +0530

    Update binary.py

--- binary.py
+++ binary.py
@@ -11,7 +11,7 @@ print("The decimal value of the number is", value)
 
 
 
-
+"""
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -19,3 +19,4 @@ print("The decimal value of the number is", value)
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+"""
