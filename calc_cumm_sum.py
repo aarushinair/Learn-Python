3d3bb520d05cedecc39b58aefa27c6ab8cf57a5e
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-01-29 00:05:32 +0530

    Update calc_cumm_sum.py

--- calc_cumm_sum.py
+++ calc_cumm_sum.py
@@ -15,7 +15,7 @@ r = np.cumsum(x,axis=1)
 print(r)
 
 
-
+"""
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -23,3 +23,4 @@ print(r)
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+"""
