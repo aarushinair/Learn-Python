82a364063b4a5843e556724611c2191243b1a1c5
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-02-09 18:03:32 +0530

    Update count_vowel.py

--- count_vowel.py
+++ count_vowel.py
@@ -24,7 +24,7 @@ print "Total vowels are: ", count
 
 
 
-
+"""
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -32,3 +32,4 @@ print "Total vowels are: ", count
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+"""
