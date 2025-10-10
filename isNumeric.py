ac3419882970785312e0509062cd47bd4d268c53
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-10-10 08:31:06 +0530

    Update isNumeric.py

--- isNumeric.py
+++ isNumeric.py
@@ -29,3 +29,18 @@ public static void main(String args[])
       }
   }
 }
+
+
+
+
+"""
+👋 Hi, I’m @aarushinair — Aarushi Nair (she/her)
+🎓 CS Engineer | AI Researcher | Software Engineer | DEI Professional
+💡 Interests: AI/ML/DL, Responsible Tech, Innovative Technologies, Ethics in AI
+🌍 Advocate for Women in Tech | Community & Events Manager @AnitaB.org India
+🎙️ Speaker | Content Creator | STEM Mentor
+📫 Let’s connect: https://www.linkedin.com/in/aarushinair/
+📹 YouTube: Code with Aarushi → https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🐦 Twitter/X: https://x.com/aarushinair_
+📁 Portfolio, projects & talks: https://github.com/aarushinair
+"""
