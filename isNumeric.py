7d768852b5f389f73d5bd6c44d3874773f6f57cb
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-10-10 08:33:49 +0530

    Update isNumeric.py

--- isNumeric.py
+++ isNumeric.py
@@ -1,34 +1,20 @@
-import java.lang.*;
-public class checkNumeric {
-public static boolean isNumeric(String str, int n)
-{
-    for (int i = 0; i &lt; n; i++) {
+def is_numeric(s: str, n: int) -> bool:
+    for i in range(n):
+        if s[i] not in '0123456789':
+            return False
+    return True
 
-        if ((str.charAt(i) == '0') ||(str.charAt(i) == '1') || (str.charAt(i)== '2') ||(str.charAt(i) == '3') ||(str.charAt(i) == '4')||(str.charAt(i) == '5';) ||(str.charAt(i) == '6') ||(str.charAt(i) =='7') ||(str.charAt(i) == '8') ||(str.charAt(i) == '9'))
-          {
-            return true;
-          }
-        else 
-          {
-            return false;
-          }
-        }
-    return false;
-}
-public static void main(String args[])
-  {
-    String str = &quot;1234&quot;;
-    int len = str.length();
-    if (isNumeric(str, len))
-      {
-        System.out.println(“String is Numeric” );
-      }
-    else
-      {
-        System.out.println( “String is not Numeric” );
-      }
-  }
-}
+def main():
+    s = "1234"
+    length = len(s)
+    if is_numeric(s, length):
+        print("String is Numeric")
+    else:
+        print("String is not Numeric")
+
+if __name__ == "__main__":
+    main()
+
 
 
 
